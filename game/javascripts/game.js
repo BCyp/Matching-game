556ab4549565ffdc3075f873f96d3c03f40f0b6c
Cyfer <bac369@nyu.edu>
2014-11-20 01:42:17 -0500

    Part 2: Done

--- game/javascripts/game.js
+++ game/javascripts/game.js
@@ -4,7 +4,7 @@
 function eleCreate( type, id, cl, content, child){
 	var newElement = document.createElement('div');
 	newElement.id = id;
-	newElement.class = cl;
+	newElement.setAttribute('class', cl);
 	newElement.textContent = content;
 
 	return newElement;
@@ -12,14 +12,14 @@ function eleCreate( type, id, cl, content, child){
 }
 
 function genCards(numPairs){
-	var symbols = [ '\u00A9', 2 , 3 , 4 , 5, 6 , 7 , 8];
+	var symbols = [ '\u00A9', '\u265E', '\u222F', '\u05EA', '\u262F' , '\u1C16', '\u267B' ,  '\u3037'];
 	var deck = [];
 	var y = 0;
 	for(var i = 0; i < numPairs*2; i++){
 
 		if( i == numPairs) y = 0;
-		var newCard = eleCreate('div', null, 'card', null, null);
-		newCard.style = 'width:70px;height:70px;border:1px solid #000; display: inline-block; float: left; margin: 5px; background-color : #305';
+		var newCard = eleCreate('div', '', 'card', null, null);
+		newCard.style = 'width:70px;height:70px;border-radius: 25px;text-align: center;vertical-align:center;line-height: 70px; box-shadow: 10px 10px 5px #888888;border:3px solid #000; display: inline-block; float: left; margin: 5px; background-color : #ff0';
 		newCard.setAttribute('symbol', symbols[y]);
 		newCard.setAttribute('match', 'false');
 		var copyCard = newCard;
@@ -76,18 +76,22 @@ function game(numPairs){
 				sC.addEventListener('click', reveal);
 			}
 			flips = 0;
-			if(matches === numPairs){
-				clearFinish();
-			}
 			guesses++;
 			updateGuess(guesses);
+			console.log('Matches = ' + matches);
+			console.log('numPairs= ' + numPairs);
+			if(matches == numPairs){
+				end();
+			}
 	};
 	var reveal = function(card){
 		this.textContent = this.getAttribute('symbol');
 		this.removeEventListener('click', reveal);
 		if(flips === 1){
 			secCard = this;
-			checkMatches(firstCard, secCard);
+			setTimeout(function() {
+				checkMatches(firstCard, secCard);	
+			}, 1000);
 		}
 		else{
 			firstCard = this;
@@ -113,16 +117,37 @@ function guessLine(){
 function updateGuess(n){
 	document.getElementById('guessLine').textContent = 'You Have Made ' + n + ' Guesses';
 };
+function end(){
+	var endLine = eleCreate( 'div', 'end', 'ender', 'THANKS 4 PALLYING BUD!!!')
+	document.getElementById('game').parentNode.insertBefore(endLine, document.getElementById('game'));
+	document.getElementById('game').remove();
+};
+function addCSS(){
+	var newElement = document.createElement('link');
+	newElement.rel = 'stylesheet';
+	newElement.href= 'styles.css';
+	document.getElementsByTagName('html')[0].insertBefore(newElement, document.getElementsByTagName('body')[0]);
+};
 
 function main(){
 	var kar = document.getElementById('numSymbols').value; 	
+	var wid = 0;
 	if(kar > 8){
 		kar = 8;
 	};
+	if(kar > 4){
+		wid = 4;
+	}
+	else{
+		wid = kar;
+	}
+	var hei = (50 * kar) + ''; 
+	var boardW = ((wid/2)*180) + '';
+	console.log(boardW);
 	document.getElementById('startForm').remove();
 	guessLine();
 	var board = eleCreate( 'div','gameBoard', null, null);
-	board.style = 'width:350px;height:700px;border:1px solid #000; margin-left: auto; margin-right: auto';
+	board.style = 'width:' + boardW + 'px;height:'+hei+'px;border:1px solid #000; border-radius: 25px; margin-left: auto; margin-right: auto';
 	document.getElementById('game').appendChild(board);
 
 	var deck = genCards(kar);
@@ -135,5 +160,6 @@ function main(){
 
 };
 
+addCSS();
 
 document.getElementById('startButton').addEventListener('click', main);
